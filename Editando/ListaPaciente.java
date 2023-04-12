c80c8ee030c05cdf5e7c023011fb33952c9f0a74
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2023-04-12 12:50:00 -0400

    Update ListaPaciente.java

--- Editando/ListaPaciente.java
+++ Editando/ListaPaciente.java
@@ -39,3 +39,16 @@ public class ListaPaciente {
         return filtrados;
     }
 }
+
+
+/*
+En esta clase se implementa una lista de pacientes utilizando un ArrayList. La clase cuenta con los siguientes métodos:
+
+ListaPaciente(): constructor de la clase que inicializa la lista de pacientes.
+getPacientes(): método que devuelve la lista de pacientes.
+agregarPaciente(Paciente paciente): método que agrega un paciente a la lista.
+buscarPaciente(String codigo): método que busca un paciente en la lista a través de su código.
+eliminarPaciente(Paciente paciente): método que elimina un paciente de la lista.
+filtrarPacientes(boolean respiratorio, String triage): método que filtra los pacientes de acuerdo a si son respiratorios o no, y su triage. Retorna una lista con los pacientes filtrados y ordenados de acuerdo a su vulnerabilidad.
+La clase ListaPaciente es utilizada en conjunto con la clase Paciente y la clase OrdenarPacientesPorVulnerabilidad para manejar la lista de pacientes y la selección de los pacientes según ciertos criterios en el sistema de atención de pacientes en la sala de urgencias.
+*/
